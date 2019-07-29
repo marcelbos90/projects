fac3319708378ed9665bea75231d4d91c8fd4ddd
Marcel Bos <marcelbos90@gmail.com>
2019-07-29 09:32:03 +0200

    initial commit

---
+++ Django_Struct/Django_Struct/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+
+from collection import views
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('admin/', admin.site.urls),
+]
