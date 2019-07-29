2d35c2dc06b1f11812c59a1b4b2d7f667c9e5232
Marcel Bos <marcelbos90@gmail.com>
2019-07-29 15:27:47 +0200

    admin added

--- Django_Struct/collection/admin.py
+++ Django_Struct/collection/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from collection.models import Project
+
+# set up automated slug creation
+class ProjectAdmin(admin.ModelAdmin):
+    model = Project
+    list_display = ('name', 'description', 'uid')
+    
+
+# and register it
+admin.site.register(Project, ProjectAdmin)
