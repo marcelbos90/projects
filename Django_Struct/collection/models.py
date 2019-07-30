c048f45fd3de91ce19192a08ef1271a467e8de53
Marcel Bos <marcelbos90@gmail.com>
2019-07-30 11:15:19 +0200

    added user attribute in Project model and project attribute in WorkPackage model

--- Django_Struct/collection/models.py
+++ Django_Struct/collection/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 import uuid
@@ -8,7 +9,11 @@ class Project(models.Model):
     slug = models.SlugField(unique=True)
     uid = str(uuid.uuid4())
 
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    
+
 class WorkPackage(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     number = models.IntegerField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, blank=True, null=True)
